9f638fc00d1c67c58f3b462cf27e2e7c8fd50bf0
SampL <samp14hd@gmail.com>
2017-09-14 00:24:07 -0300

    update (#8)
    * Sequencial #3
    
    * oioi
    
    * oioioioioioioi
    
    * oi
    
    * ;...
    
    * até o #3
    
    * updated
    
    * ..
    
    * ..
    
    * .......

--- SampaioLeal/PHP/Sequencial/Programa.php
+++ SampaioLeal/PHP/Sequencial/Programa.php
@@ -18,19 +18,40 @@ class Sequencial
         echo ("Seu telefone é $tel");
         session_destroy();
     }
-    function somar($num1, $num2)
-    {
-        $_SESSION['page'] = "#3";
-        echo ($num1 + $num2);
-        session_destroy();
-    }
     function showNum()
     {
         $_SESSION['page'] = "#3";
-            echo ($_POST['num']);
-            session_destroy();
+        echo ($_POST['num']);
+        session_destroy();
     }
-
+    function somar($num1, $num2)
+    {
+        $_SESSION['page'] = "#4";
+        echo ($num1 + $num2);
+        session_destroy();
+    }
+    function calcMedia($bim1, $bim2, $bim3, $bim4)
+    {
+        $_SESSION['page'] = "#5";
+        $soma = $bim1 + $bim2 + $bim3 + $bim4;
+        $media = $soma/4;
+        echo ($media);
+        session_destroy();
+    }
+    function showBrothers($filho)
+    {
+        $_SESSION['page'] = "#6";
+        $irmao = $filho - 1;
+        $irma = $filho + 1;
+        echo ($irmao. " e " .$irma);
+        session_destroy();
+    }
+    function showQuadradinho($num)
+    {
+        $_SESSION['page'] = "#8";
+        echo($num*$num);
+        session_destroy();
+    }
     function showPage()
     {
         if (isset($_SESSION['page'])) {
--- SampaioLeal/PHP/Sequencial/index.php
+++ SampaioLeal/PHP/Sequencial/index.php
@@ -37,10 +37,8 @@
                 <a id="i4" class="item">#4</a>
                 <a id="i5" class="item">#5</a>
                 <a id="i6" class="item">#6</a>
-                <a id="i7" class="item">#7</a>
+
                 <a id="i8" class="item">#8</a>
-                <a id="i9" class="item">#9</a>
-                <a id="i10" class="item">#10</a>
             </div>
             <br><br><br>
         </div>
@@ -117,14 +115,92 @@
                     </div>
                     <h2>
                         <?php
-                            if (isset($_POST['calc'])) {
-                                 $programa->somar($_POST['calc1'], $_POST['calc2']);
-                            } else {
-                                    }
+                        if (isset($_POST['calc'])) {
+                            $programa->somar($_POST['calc1'], $_POST['calc2']);
+                        } else {
+                        }
                         ?>
                     </h2>
                 </center>
             </div>
+            <div id="5" class="conteudo" hidden>
+                <center>
+                    <h1>Média do Semestre</h1>
+                    <div class="ui grid center aligned">
+                        <form class="ui form" id="calculadora" method="post">
+                            <div class="field">
+                                <label>1º Bimestre</label>
+                                <input type="number" name="bim1" title="Digite a nota" required>
+                            </div>
+                            <div class="field">
+                                <label>2º Bimestre</label>
+                                <input type="number" name="bim2" title="Digite a nota" required>
+                            </div>
+                            <div class="field">
+                                <label>3º Bimestre</label>
+                                <input type="number" name="bim3" title="Digite a nota" required>
+                            </div>
+                            <div class="field">
+                                <label>4º Bimestre</label>
+                                <input type="number" name="bim4" title="Digite a nota" required>
+                            </div>
+                            <input class="ui green button" name="bim" type="submit">
+                        </form>
+                    </div>
+                    <h2>
+                        <?php
+                        if (isset($_POST['bim'])) {
+                            $programa->calcMedia($_POST['bim1'], $_POST['bim2'], $_POST['bim3'], $_POST['bim4']);
+                        } else {
+                        } 
+                        ?>
+                    </h2>
+                </center>
+            </div>
+            <div id="6" class="conteudo" hidden>
+                <center>
+                    <h1>Irmão e Irmã</h1>
+                    <div class="ui grid center aligned">
+                        <form class="ui form" id="calculadora" method="post">
+                            <div class="field">
+                                <input type="number" name="filho" placeholder="Qualquer número" required>
+                            </div>
+                            <input class="ui green button" name="filho-submit" type="submit">
+                        </form>
+                        <br>
+
+                    </div>
+                    <h2>
+                        <?php if (isset($_POST['filho-submit'])) {
+                                $programa->showBrothers($_POST['filho']);
+                            } ?>
+                    </h2>
+                </center>
+            </div>
+            <div id="8" class="conteudo" hidden>
+                <center>
+                    <h1>Faz quadradinho de <?php if (isset($_POST['num-quadrado'])){echo $_POST['numquadrado'];} else { echo("{seu numero aqui}"); } ?></h1>
+                    <div class="ui grid center aligned">
+                        <form class="ui form" id="calculadora" method="post">
+                            <div class="field">
+                                <input type="number" name="numquadrado" placeholder="Qualquer número" required>
+                            </div>
+                            <input class="ui green button" name="num-quadrado" type="submit">
+                        </form>
+                        <br>
+
+                    </div>
+                    <h2>
+                        <?php if (isset($_POST['num-quadrado'])) {
+                                $programa->showQuadradinho($_POST['numquadrado']);
+                            } ?>
+                    </h2>
+                </center>
+            </div>
+        </div>
+
+        <script src="assets/jquery-3.2.1.js"></script>
+        <script src="assets/pages.js"></script>
         </div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
